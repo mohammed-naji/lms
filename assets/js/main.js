55ff3b322700caf1d5d4143c01ec0d091c929e45
Mohammed <m.naji1512@gmail.com>
2021-12-01 12:28:51 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 var menu_btn = document.querySelector('.navbar-toggler');
-menu_btn.onclick = e => document.querySelector(menu_btn.dataset.bsTarget).classList.toggle('show')
+menu_btn.onclick = e => document.querySelector(menu_btn.dataset.bsTarget).classList.toggle('show');
+
+document.querySelectorAll('li.nav-item.has-dropdown').forEach(function(li) {
+    console.log(li);
+    li.addEventListener('click', function() {
+        this.classList.toggle('active');
+    })
+})
